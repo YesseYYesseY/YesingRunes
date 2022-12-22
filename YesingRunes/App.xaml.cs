9bd3b2c3694d830a4a090132eb8b53e99a6fbffe
YesseYYesseY <yeskommeryesdig@gmail.com>
2022-12-22 09:41:05 +0100

    Fix BuildTools

--- YesingRunes/App.xaml.cs
+++ YesingRunes/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -19,19 +21,29 @@ namespace YesingRunes
         {
             var processes = Process.GetProcessesByName("YesingRunes");
             if (processes.Length > 1) Environment.Exit(0);
-            if (!Directory.Exists(Directory.GetCurrentDirectory() + "./Data") ||
-                !File.Exists(Directory.GetCurrentDirectory() + "./Data/version.json"))
+            if(File.Exists(Directory.GetCurrentDirectory() + "./Data/version.json"))
             {
-                Process buildTools = new Process()
+                var client = new HttpClient();
+                var res = client.Send(new HttpRequestMessage(HttpMethod.Get, "https://ddragon.leagueoflegends.com/api/versions.json"));
+                var versions = JsonSerializer.Deserialize<string[]>(res.Content.ReadAsStringAsync().Result);
+                if(versions is not null && versions.Length > 0)
                 {
-                    StartInfo = new ProcessStartInfo()
+                    if(File.ReadAllText(Directory.GetCurrentDirectory() + "./Data/version.json") == versions[0])
                     {
-                        FileName = Directory.GetCurrentDirectory() + "./BuildTools.exe"
+                        return;
                     }
-                };
-                buildTools.Start();
-                buildTools.WaitForExit();
+                }
             }
+
+            Process buildTools = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Directory.GetCurrentDirectory() + "./BuildTools.exe"
+                }
+            };
+            buildTools.Start();
+            buildTools.WaitForExit();
         }
     }
 }
